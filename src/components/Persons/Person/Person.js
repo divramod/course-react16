13f8f0d3cb0ce04868d3c54e591594b3c8e12630
divramod <divramod@gmail.com>
2018-12-14 05:42:46 +0100

    97

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import classes from './Person.css';
+import Aux from './../../../hoc/Aux';
+import withClass from './../../../hoc/withClass';
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[Person.js] Inside Constructor');
+    }
+
+    componentWillMount() {
+        console.log('[Person.js] Inside componentWillMount()');
+    }
+
+    componentDidMount() {
+        console.log('[Person.js] Inside componentDidMount()');
+    }
+
+    componentWillUnmount() {
+        console.log('[Person.js] Inside componentWillUnmount()');
+    }
+
+    render() {
+
+        console.log('[Person.js] Inside render()');
+
+        return (
+            <Aux>
+                <button onClick={this.props.click} >Delete</button>
+                <p>I'm {this.props.name}</p>
+                <p>I'm {this.props.age}</p>
+                <p>{this.props.children}</p>
+                <input onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+        )
+    }
+}
+export default withClass(Person, classes.Person);
---
+++ src/containers/App.js
@@ -0,0 +1,110 @@
+import React, { PureComponent } from 'react';
+import classes from './App.css';
+import Persons from './../components/Persons/Persons';
+import Cockpit from './../components/Cockpit/Cockpit';
+import Aux from './../hoc/Aux';
+import withClass from './../hoc/withClass';
+
+class App extends PureComponent {
+
+    constructor(props) {
+        super(props);
+        console.log('[App.js] Inside Constructor');
+    }
+
+    componentWillMount() {
+        console.log('[App.js] Inside componentWillMount()');
+    }
+
+    componentDidMount() {
+        console.log('[App.js] Inside componentDidMount()');
+    }
+
+    // shouldComponentUpdate ( nextProps, nextState ) {
+    // console.log(
+    // '[Update App.js] Inside shouldComponentUpdate()',
+    // nextProps,
+    // nextState
+    // );
+    // // return true;
+    // return nextState.persons !== this.state.persons ||
+    // nextState.showPersons !== this.state.showPersons;
+    // }
+
+    componentWillUpdate ( nextProps, nextState ) {
+        console.log(
+            '[Update App.js] Inside componentWillUpdate()',
+            nextProps,
+            nextState
+        );
+    }
+
+    componentDidUpdate () {
+        console.log('[Update App.js] Inside componentDidUpdate()');
+    }
+
+    state = {
+        persons: [
+            {id:'1b', name: 'Max', age: 28},
+            {id:'1a', name: 'Man', age: 29},
+            {id:'1c', name: 'Maa', age: 30}
+        ],
+        otherState: 'some other ',
+        showPersons: false,
+    }
+
+    togglePersonsHandler = () => {
+        console.log("in");
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    }
+
+    deletePersonHandler = (personsIndex) => {
+        const persons = [...this.state.persons];
+        persons.splice(personsIndex, 1)
+        this.setState({persons})
+    }
+
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex( p => {
+            return p.id === id;
+        });
+        const person = { ...this.state.persons[personIndex] };
+        person.name = event.target.value;
+        const persons = [...this.state.persons]
+        persons[personIndex] = person
+        this.setState({persons})
+    }
+
+    render() {
+        console.log('[App.js] Inside render()');
+        let persons = null
+        if ( this.state.showPersons === true ) {
+            persons = (
+                <Persons
+                    persons={this.state.persons}
+                    clicked={this.deletePersonHandler}
+                    changed={this.nameChangedHandler}
+                />
+            )
+        }
+
+        return (
+            <Aux>
+                <button
+                    type="text"
+                    onClick={() => {this.setState({showPersons: true})}}>
+                    Show Persons
+                </button>
+                <Cockpit
+                    appTitle={this.props.title}
+                    showPersons={this.state.showPersons}
+                    persons={this.state.persons}
+                    clicked={this.togglePersonsHandler}
+                />
+                {persons}
+            </Aux>
+        );
+    }
+}
+export default withClass(App, classes.App);
